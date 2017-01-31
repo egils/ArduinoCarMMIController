1d4e84af74a48709259ddf7d31268b9e0326c6dc
Egidijus Lukauskas <egils.ps@gmail.com>
2017-01-31 23:23:34 +0200

    add latest reverse engineered bus codes and messages

--- src/main.ino
+++ src/main.ino
@@ -2,11 +2,26 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
-#define BLE_ENABLED true
-#define CAN_BUS_ENABLED false
+#define BLE_ENABLED false
+#define CAN_BUS_ENABLED true
+
+#define RING 0x428
+#define DEVICE 0x436
+#define AUDIO_SOURCE 0x661
 
 #define DIS_TEXT_line1 0x261
 #define DIS_TEXT_line2 0x263
+
+unsigned char requestToJoin[6] = {0x16, 0x02, 0xC0, 0x04, 0x00, 0x00};
+unsigned char okToJoin[6] = {0x16, 0x01, 0x00, 0x00, 0x00, 0x00};
+unsigned char ack[6] = {0x08, 0x01, 0x00, 0x00, 0x00, 0x00};
+unsigned char fmRadio[8] = {0x81, 0x01, 0x12, 0xA0, 0x00, 0x00, 0x00, 0x00};
+
+unsigned char testMessage[8] = {0x45, 0x4C, 0x54, 0x4F, 0x4E, 0x20, 0x4A, 0x4F};
+
+unsigned char length = 0;
+unsigned char buf[8];
+char str[20];
 
 MCP_CAN CAN(10);
 SoftwareSerial BTSerial(5, 6); // RX | TX
@@ -15,36 +30,51 @@ String currentMessage;
 
 void setup() {
   // Init BLuetooth (BLE) and debugging Serial
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   if (BLE_ENABLED) {
-    BTSerial.begin(9600);
+    BTSerial.begin(115200);
   }
 
   // Init CANBUS
   if (CAN_BUS_ENABLED) {
-    unsigned int status;
+    START_INIT:
 
-    do {
-      status = CAN.begin(CAN_500KBPS);
-      delay(100);
-    } while (CAN_OK == status);
+    if(CAN_OK == CAN.begin(CAN_200KBPS))                   // init can bus : baudrate = 100k
+    {
+        Serial.println("CAN BUS Shield init ok!");
+    }
+    else
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println("Init CAN BUS Shield again");
+        delay(100);
+        goto START_INIT;
+    }
 
-    Serial.println("* CANBUS init successful!");
+    CAN.sendMsgBuf(DEVICE, 0, 6, requestToJoin);
+    delay(100);
+    CAN.sendMsgBuf(DEVICE, 0, 6, ack);
   }
 }
 
 void loop() {
-  if (BLE_ENABLED) {
-    handleBLEmessages();
+  if (CAN_BUS_ENABLED) {
+    CAN.sendMsgBuf(AUDIO_SOURCE, 0, 8, fmRadio);
+    delay(100); // should report status only once in 100ms
+
+    CAN.sendMsgBuf(DIS_TEXT_line1, 0, 8, testMessage);
+
+    // readCANMessage();
   }
 
-  if (CAN_BUS_ENABLED) {
-    readAllCANMessages();
+  if (BLE_ENABLED) {
+    handleBLEmessage();
   }
+
 }
 
-void handleBLEmessages() {
+void handleBLEmessage() {
   if(BTSerial.available()) {
     currentMessage = BTSerial.readString();
   }
@@ -61,6 +91,7 @@ void handleBLEmessages() {
     }
 
     currentMessage.remove(0, bufSize);
+
   }
 
 
@@ -72,17 +103,29 @@ void handleBLEmessages() {
   }
 }
 
-void readAllCANMessages() {
-  unsigned char length = 0;
-  unsigned char buf[8];
+void readCANMessage() {
+  unsigned char buffer[8];
+  INT32U canId = 0x000;
 
   if(CAN_MSGAVAIL == CAN.checkReceive()) {
-    CAN.readMsgBuf(&length, buf);
+    CAN.readMsgBuf(&length, buffer);
+
+    canId = CAN.getCanId();
+
+    Serial.print(canId, HEX); Serial.print('>');
 
     for(int i = 0; i<length; i++) {
-      Serial.write(buf[i]);
-      Serial.write("\t");
+      if (buffer[i]<16) {
+        Serial.print('0');
+      }
+
+      Serial.print(buffer[i],HEX);
+
+      if (i<length-1) {
+        Serial.print(":");
+      }
     }
+
     Serial.println();
   }
 }
